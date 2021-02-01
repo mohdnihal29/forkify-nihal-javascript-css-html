128081d6abe26a3cbb24b0c9dea89a02d825c47a
mohdnihal29 <mohdnihal29@gmail.com>
2021-02-01 15:16:27 +1100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '51a062e6-e01d-45a7-8320-59d6120511f7';
+export const MODAL_CLOSE_SEC = 2.5;
